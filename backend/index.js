717e757f5d9bbae40fa418e041799a6a6644c494
Sandep Painkra <samdeeppainkra@gmail.com>
2022-12-10 01:48:12 +0530

    updating backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
+const DB = process.env.DB;
 const PORT = 5000 || process.env.PORT;
 
+mongoose.connect(
+  DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (req, res) => {
+    console.log("Connection successfully");
+  }
+);
 app.get("/", (req, res) => {
   res.send(
     "hello world this is backend practise of Ridesharing and carpooling app"
